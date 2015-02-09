c1b971c48ec53506fcc1c3eb4cb27216ca247bac
Kurtis Jensen <jkurtis@concol.cc>
2015-02-08 21:22:10 -0500

    templates

--- parts/page/page.py
+++ parts/page/page.py
@@ -1,23 +1,39 @@
 import logging
 log = logging.getLogger(__name__)
 
-from fapengine.fedj.parts import Part
+from djangoParts.parts import Part
 
 class Page(Part):
     TEMPLATE_PATH = "parts/page.html"
+    CONTENTS_PART = None
+
+    def __init__(self):
+        Part.__init__(self)
+        self.check()
+        self.gatherRequirements()
+
+    def check(self):
+        if self.CONTENTS_PART == None:
+            raise Exception("your page has not been given any self.CONTENTS_PART")
+
+    def gatherRequirements(self):
+        self.css_objects = []
+        for css_class in self.REQUIRE_CSS:
+            self.css_objects.append(css_class())
+        self.js_objects = []
+        for js_class in self.REQUIRE_JS:
+            self.js_objects.append(js_class())
+
     
     def fetch(self,request,*args,**kwargs):
-        css = self.REQUIRE_CSS
-        js = self.REQUIRE_JS
-
         css_html = ""
-        for css_class in css:
-            css_html += css_class().render(request)
+        for css_class in self.css_objects:
+            css_html += css_class.render(request)
         js_html = ""
-        for js_class in js:
-            js_html += js_class().render(request)
+        for js_class in self.js_objects:
+            js_html += js_class.render(request)
 
-        contents_html = self.getContents(request,*args,**kwargs)
+        contents_html = self.CONTENTS_PART().render(request,*args,**kwargs)
         return {
                 "page_css":css_html,
                 "page_js":js_html,
--- parts/part/part.py
+++ parts/part/part.py
@@ -4,14 +4,51 @@ from django.http import HttpResponse
 from django.views.generic import View
 
 class Part(View):
+    NAME = None
+    TEMPLATE_PATH = None
+
+    def __init__(self):
+        self.check()
+
+    def check(self):
+        if self.NAME == None:
+            raise Exception("tried to create a part without a self.NAME")
+        if self.TEMPLATE_PATH == None:
+            raise Exception("tried to create a part without a self.TEMPLATE_PATH")
+
+
     def get(self,request,*args,**kwargs):
-        return HttpResponse(self.render(request,*args,**kwargs))
+        """
+        takes arguments from url
+        returns response with rendered html
+        """
+        response = HttpResponse(self.render(request,*args,**kwargs))
+        response.__setitem__("Access-Control-Allow-Origin", "*")
+        return response
 
     def render(self,request,*args,**kwargs):
+        """
+        takes arguments
+        fetches data
+        reutrns rendered html
+        """
         context = self.fetch(request,*args,**kwargs)
         #TODO: check that context is a {} type
         context_inst = RequestContext(request,context)
-        return render_to_string(self.TEMPLATE_PATH,context,context_inst)
+        content = render_to_string(self.TEMPLATE_PATH,context,context_inst)
+
+        part_context = {
+                        "name":self.NAME,
+                        "content":content,
+                    }
+        return render_to_string("parts/part.html",part_context,context_inst)
 
     def fetch(self,request,*args,**kwargs):
+        """
+        takes arguments
+        gets data from source
+        returns context
+
+        should usually be overridden
+        """
         return {}
